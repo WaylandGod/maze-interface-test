e7804af6debcd9e91781ec075d6ec324fd80b079
Thorsten Sommer <service@sommer-engineering.com>
2017-02-06 21:05:40 +0100

    Added agent simulation

--- Golang/main.go
+++ Golang/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
 	"net/http"
@@ -54,8 +55,71 @@ func main() {
 		}
 	})
 
+	//
+	// A simulation for the agent i.e. AI interface. It reads from stdin and forwards
+	// the data to the HTTP channel towards the visualization.
+	//
+	// Please notice:
+	// This implementation will create a new simulation for each HTTP request. Thus, each
+	// client gets his own simulator. This behaviour is maybe not desired for the final implementation.
+	//
 	serverMUX.HandleFunc("/agent", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %s", "test")
+
+		// Create a reader and connect it to stdin:
+		stdinReader := bufio.NewReader(os.Stdin)
+
+		for {
+			// Read the next line i.e. action of the agent:
+			line, _ := stdinReader.ReadString('\n')
+
+			// Forward the data to the HTTP channel:
+			fmt.Fprintf(w, "%s\n", line)
+
+			// Flush the data in order to use the HTTP channel as never ending stream:
+			if f, ok := w.(http.Flusher); ok {
+				f.Flush()
+			} else {
+				fmt.Println("Damn, no flush")
+			}
+
+			//
+			// TODO: Use the data to calculate some reaction of the maze...
+			//
+
+			//
+			// Simulate the reaction:
+			//
+
+			// 6 Range finder sensors i.e. distance to the next walls:
+			range1 := rand.Intn(100)
+			range2 := rand.Intn(100)
+			range3 := rand.Intn(100)
+			range4 := rand.Intn(100)
+			range5 := rand.Intn(100)
+			range6 := rand.Intn(100)
+
+			// 4 Pie-Slice Sensors i.e. angel and distance to goal (if visible):
+			pieSlice1Angel := rand.Intn(361)
+			pieSlice1Dist := rand.Intn(100)
+
+			pieSlice2Angel := rand.Intn(361)
+			pieSlice2Dist := rand.Intn(100)
+
+			pieSlice3Angel := rand.Intn(361)
+			pieSlice3Dist := rand.Intn(100)
+
+			pieSlice4Angel := rand.Intn(361)
+			pieSlice4Dist := rand.Intn(100)
+
+			// Create the reaction string:
+			reaction := fmt.Sprintf("%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d\n", range1, range2, range3, range4, range5, range6, pieSlice1Angel, pieSlice1Dist, pieSlice2Angel, pieSlice2Dist, pieSlice3Angel, pieSlice3Dist, pieSlice4Angel, pieSlice4Dist)
+
+			// Write it to stdout i.e. to the agent / A.I:
+			fmt.Print(reaction)
+
+			// Write it to the HTTP channel:
+			// TODO
+		}
 	})
 
 	// Define the HTTP server:
